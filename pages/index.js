67c32478d2d67681e169d20a7d2da8f48309221a
thatSchloesser <sschloesser2@gmail.com>
2021-01-08 14:13:24 -0500

    SWR client data fetching skeleton example

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,16 @@ import Link from 'next/Link';
 
 import { getSortedPostsData } from '../lib/posts';
 
+// import useSWR from 'swr';
+
+// function Profile() {
+//   const { data, error } = useSWR('/api/user', fetch);
+
+//   if (error) return <div>failed to load</div>;
+//   if (!data) return <div>loading...</div>;
+//   return <div>hello {data.name}!</div>;
+// }
+
 //data dependencies for PRE Rendering
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
