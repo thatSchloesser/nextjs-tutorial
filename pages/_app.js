a4fa33e5643e1ed626e6f2d9d413382f5a8fb8e5
thatSchloesser <sschloesser2@gmail.com>
2021-01-14 18:04:57 -0500

    comments

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 //global styles
 import '../styles/global.css';
 
+//this top level component allows us to use global styles
+//AND keep state when navigating between pages if desired.
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
