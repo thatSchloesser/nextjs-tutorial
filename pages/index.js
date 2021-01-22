975f21cad70a824140c6d8e541a073d44081994d
thatSchloesser <sschloesser2@gmail.com>
2021-01-22 15:01:18 -0500

    import next/Link -> next/link for linux deployment

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import Link from 'next/Link';
+import Link from 'next/link';
 import Date from '../components/date';
 
 import { getSortedPostsData } from '../lib/posts';
